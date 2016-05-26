c6f84060d5f55df66ac63d144fa3c63785774f39
jcao14 <jcao@stuy.edu>
2016-05-26 14:49:27 -0400

    Create RunMed.java

---
+++ RunMed.java
@@ -0,0 +1,91 @@
+/*****************************************************
+ * class RunMed
+ * Implements an online algorithm to track the median of a growing dataset
+ * Maintains 2 heaps: minheap for upper half of dataset, maxheap for lower half
+ * Median will always be one of these:
+ *  - max of left heap  (lower range)
+ *  - min of right heap (upper range)
+ *  - mean of heap roots
+ *****************************************************/
+
+public class RunMed {
+
+    //instance vars
+    private ALMaxHeap leftHeap;  //for lower range of dataset
+    private ALMinHeap rightHeap; //for upper range of dataset
+
+
+    /*****************************************************
+     * default constructor  ---  inits empty heap
+     *****************************************************/
+    public RunMed() 
+    { 
+
+    }//O(1)
+
+
+
+    /*****************************************************
+     * double getMedian()  ---  returns median of dataset
+     *****************************************************/
+    public double getMedian() 
+    {
+
+    }//O(1)
+
+
+
+    /*****************************************************
+     * insert(int)  ---  adds a new element to the dataset
+     * postcondition: dataset is maintained such that 
+     *                getMedian() can run in constant time
+     *****************************************************/
+    public void insert( int addVal )
+    {   
+     }//O(?)
+
+
+
+    /*****************************************************
+     * boolean isEmpty()  ---  tells whether a median may be calculated
+     * postcondition: dataset structure unchanged
+     *****************************************************/
+    public boolean isEmpty() 
+    {
+
+    }//O(?)
+
+
+
+    //main method for testing
+    public static void main( String[] args ) {
+
+	/*~~~V~~~~~~~~~~~~move~me~down~~~~~~~~~~~~~V~~~
+        RunMed med = new RunMed();
+
+        med.insert(1);
+	System.out.println( med.getMedian() ); //1
+        med.insert(3);
+	System.out.println( med.getMedian() ); //2
+        med.insert(5);
+	System.out.println( med.getMedian() ); //3
+        med.insert(7);
+	System.out.println( med.getMedian() ); //4
+        med.insert(9);
+	System.out.println( med.getMedian() ); //5
+	~~~~~|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|~~~*/
+
+    }//end main()
+
+}//end class RunMed
+
+
+
+    /*****************************************************
+     * 
+     *****************************************************/
+    // (  )
+    // {
+    // 	/*** YOUR IMPLEMENTATION HERE ***/
+    // }//O(?)
+
